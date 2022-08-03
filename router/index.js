f55304be2aa81de26d21828fdbf157f5eed4349b
Anton Sylka <anton.sylka@gorbunov.studio>
2022-08-03 13:00:42 +0800

    Refs #26999: Disable unused Vuex modules and remove unused routes

--- router/index.js
+++ router/index.js
@@ -10,8 +10,6 @@ const PillowProduct = () =>
   import(/* webpackChunkName: "vsf-pillow-product" */ 'theme/pages/PillowProduct');
 const PhrasePillowProduct = () =>
   import(/* webpackChunkName: "vsf-phrase-pillow-product" */ 'theme/pages/PhrasePillowProduct');
-const Typography = () =>
-  import(/* webpackChunkName: "vsf-product" */ 'theme/pages/Typography');
 const Category = () =>
   import(/* webpackChunkName: "vsf-category" */ 'theme/pages/Category');
 const Checkout = () =>
@@ -20,8 +18,6 @@ const DetailedCart = () =>
   import(/* webpackChunkName: "vsf-detailed-cart" */ 'theme/pages/DetailedCart');
 const MyAccount = () =>
   import(/* webpackChunkName: "vsf-my-account" */ 'theme/pages/MyAccount');
-const Static = () =>
-  import(/* webpackChunkName: "vsf-static" */ 'theme/pages/Static');
 const CartRecovery = () =>
   import(/* webpackChunkName: "vsf-cart-recovery" */ 'theme/pages/CartRecovery');
 const CrossSells = () =>
@@ -65,21 +61,9 @@ let routes = [
       }
     })
   },
-  { name: 'legal', path: '/legal/', component: Static },
-  { name: 'privacy', path: '/privacy/', component: Static },
   { name: 'my-account', path: '/my-account/', component: MyAccount },
-  { name: 'about-us', path: '/about-us/', component: Static },
-  { name: 'customer-service', path: '/customer-service/', component: Static },
-  { name: 'store-locator', path: '/store-locator/', component: Static },
-  { name: 'size-guide', path: '/size-guide/', component: Static },
-  { name: 'delivery', path: '/delivery/', component: Static },
-  { name: 'returns', path: '/returns/', component: Static },
-  { name: 'contact', path: '/contact/', component: Static },
-  { name: 'typography-text', path: '/typography-text/', component: Typography },
-  { name: 'typography-form', path: '/typography-form/', component: Typography },
-  { name: 'typography-other', path: '/typography-other/', component: Typography },
   { name: 'page-not-found', path: '*', component: ErrorPage },
-  { name: 'error', path: '/error', component: ErrorPage, meta: { layout: 'minimal' } },
+  { name: 'error', path: '/error/', component: ErrorPage, meta: { layout: 'minimal' } },
   { name: 'virtual-product', path: '/p/:parentSku/:slug/', component: Product },
   { name: 'bundle-product', path: '/p/:parentSku/:slug/', component: Product },
   { name: 'simple-product', path: '/p/:parentSku/:slug/', component: Product },
@@ -90,7 +74,6 @@ let routes = [
   { name: 'petsiesStarProduct-product', path: '/p/:parentSku/:slug/', component: Product },
   { name: 'product', path: '/p/:parentSku/:slug/:childSku/', component: Product },
   { name: 'category', path: '/c/:slug/', component: Category },
-  { name: 'cms-page', path: '/i/:slug/', component: Static },
   {
     name: 'forevers-create',
     path: '/forevers/create/',
