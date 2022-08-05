7ce07faed19bde894a0c6653e91670c13a9c2024
Anton Sylka <anton.sylka@gorbunov.studio>
2022-08-05 17:09:54 +0800

    Refs #27113: MNotifyCustomerSupportForm - implement component

---
+++ components/molecules/m-notify-customer-support-form.vue
@@ -0,0 +1,65 @@
+<template>
+  <form @submit.prevent="onFormSubmit" class="m-notify-customer-support-form">
+    <SfInput v-model="name" name="name" :placeholder="$t('Your name')" />
+    <SfInput v-model="email" name="email" :placeholder="$t('Your email')" />
+    <SfInput v-model="phone" name="name" :placeholder="$t('Phone (optional)')" />
+
+    <SfButton class="sf-button sf-button--outline">
+      {{ $t('Notify Customer Support') }}
+    </SfButton>
+  </form>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import { SfInput, SfButton } from '@storefront-ui/vue';
+
+export default Vue.extend({
+  name: 'MNotifyCustomerSupportForm',
+  components: {
+    SfInput,
+    SfButton
+  },
+  props: {
+    prefilledName: {
+      type: String,
+      default: undefined
+    },
+    prefilledEmail: {
+      type: String,
+      default: undefined
+    },
+    action: {
+      type: Function as PropType<(name: string, email: string, phone?: string) => Promise<void>>,
+      required: true
+    }
+  },
+  data () {
+    return {
+      email: '',
+      name: '',
+      phone: '',
+      isSubmitting: false
+    }
+  },
+  beforeMount () {
+    this.email = this.prefilledEmail;
+    this.name = this.prefilledName;
+  },
+  methods: {
+    async onFormSubmit () {
+      if (this.isSubmitting) {
+        return;
+      }
+
+      this.isSubmitting = true;
+
+      try {
+        await this.action(this.name, this.email, this.phone);
+      } finally {
+        this.isSubmitting = false;
+      }
+    }
+  }
+})
+</script>
