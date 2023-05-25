b2d0378fd14b185bbd9203ecfb09b9073d5a08b0
Anton Sylka <antonsylka1004@gmail.com>
2023-05-25 16:26:33 +0700

    Refs #29310: MPajamaStyleSelector - fix indents

--- components/organisms/OPajamaProductOrderForm/m-pajama-style-selector.vue
+++ components/organisms/OPajamaProductOrderForm/m-pajama-style-selector.vue
@@ -96,91 +96,91 @@ $bodypart-item-width: 115px;
 $color-selector-selected-border-color: #51a7f9;
 
 .m-pajama-style-selector {
-    list-style: none;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: flex-start;
-    padding: 0;
-    row-gap: 2vw;
-    text-align: center;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  padding: 0;
+  row-gap: 2vw;
+  text-align: center;
 
-    &.-disabled {
+  &.-disabled {
     ._visual-selector {
       opacity: 0.7;
 
-      > label {
-       cursor: default;
+      >label {
+        cursor: default;
       }
     }
   }
 
-    ._value {
-      box-sizing: border-box;
-      width: 33.33%;
-      padding: 0 var(--spacer-sm);
-      max-width: $bodypart-item-width;
+  ._value {
+    box-sizing: border-box;
+    width: 33.33%;
+    padding: 0 var(--spacer-sm);
+    max-width: $bodypart-item-width;
 
-      >input {
-        display: block;
-        height: 0;
-        opacity: 0;
-        width: 0;
-        margin: 0;
-        padding: 0;
-        border: 0;
+    >input {
+      display: block;
+      height: 0;
+      opacity: 0;
+      width: 0;
+      margin: 0;
+      padding: 0;
+      border: 0;
     }
 
     >label {
-        height: auto;
-        margin: 0;
-        min-height: 0;
-        padding: 0;
-        width: 100%;
-        cursor: pointer;
+      height: auto;
+      margin: 0;
+      min-height: 0;
+      padding: 0;
+      width: 100%;
+      cursor: pointer;
     }
 
     >input:checked+label ._icon::before {
-        background: rgba(52, 184, 147, 0.7);
-        border-radius: 100%;
-        content: "";
-        height: 100%;
-        left: 0;
-        position: absolute;
-        top: 0;
-        width: 100%;
-        z-index: 1;
+      background: rgba(52, 184, 147, 0.7);
+      border-radius: 100%;
+      content: "";
+      height: 100%;
+      left: 0;
+      position: absolute;
+      top: 0;
+      width: 100%;
+      z-index: 1;
     }
 
     >input:checked+label ._icon::after {
-        background: url('/assets/images/sprite/ico-tick-green.png') no-repeat center #fff;
-        border: 2px solid #38b677;
-        border-radius: 100%;
-        content: "";
-        height: 24px;
-        position: absolute;
-        right: 0;
-        top: 0;
-        width: 24px;
-        z-index: 2;
+      background: url('/assets/images/sprite/ico-tick-green.png') no-repeat center #fff;
+      border: 2px solid #38b677;
+      border-radius: 100%;
+      content: "";
+      height: 24px;
+      position: absolute;
+      right: 0;
+      top: 0;
+      width: 24px;
+      z-index: 2;
     }
-    }
+  }
 
-    ._icon {
-        background-position: center center;
-        background-repeat: no-repeat;
-        background-size: contain;
-        border-radius: 50%;
-        padding-top: 100%;
-        width: 100%;
-        position: relative;
-        z-index: 0;
-    }
+  ._icon {
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: contain;
+    border-radius: 50%;
+    padding-top: 100%;
+    width: 100%;
+    position: relative;
+    z-index: 0;
+  }
 
-    ._name {
-        font-size: var(--font-xs);
-        font-weight: var(--font-medium);
-        margin-top: var(--spacer-sm);
-        text-transform: capitalize;
-    }
+  ._name {
+    font-size: var(--font-xs);
+    font-weight: var(--font-medium);
+    margin-top: var(--spacer-sm);
+    text-transform: capitalize;
+  }
 }
 </style>
