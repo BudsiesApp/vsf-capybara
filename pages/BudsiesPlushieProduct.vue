85364b5a29beb2df9bdbf15c0dcf381c1c9e3122
Anton Sylka <antonsylka1004@gmail.com>
2023-07-26 15:56:35 +0700

    Refs #30035: BudsiesPlushieProduct - implement component

---
+++ pages/BudsiesPlushieProduct.vue
@@ -0,0 +1,146 @@
+<template>
+  <div id="budsies-plushie-product" itemscope itemtype="http://schema.org/Product">
+    <o-budsies-plushie-product-order-form
+      :artwork-upload-url="artworkUploadUrl"
+      :product="getCurrentProduct"
+      :plushie-id="plushieId"
+      @make-another="onMakeAnother"
+      v-if="getCurrentProduct"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import config from 'config';
+import { htmlDecode } from '@vue-storefront/core/filters';
+import { isServer } from '@vue-storefront/core/helpers';
+import { catalogHooksExecutors } from '@vue-storefront/core/modules/catalog-next/hooks';
+import { PRODUCT_UNSET_CURRENT } from '@vue-storefront/core/modules/catalog/store/product/mutation-types';
+
+import Product from 'core/modules/catalog/types/Product';
+
+import OBudsiesPlushieProductOrderForm from '../components/organisms/o-budsies-plushie-product-order-form.vue';
+
+export default Vue.extend({
+  name: 'BudsiesPlushieProduct',
+  props: {
+    sku: {
+      type: String,
+      required: true
+    },
+    existingPlushieId: {
+      type: String,
+      default: ''
+    }
+  },
+  components: {
+    OBudsiesPlushieProductOrderForm
+  },
+  data () {
+    return {
+      plushieId: undefined as number | undefined
+    };
+  },
+  computed: {
+    getCurrentProduct (): Product | null {
+      const product = this.$store.getters['product/getCurrentProduct'];
+
+      if (product?.sku !== this.sku) {
+        return null;
+      }
+
+      return product;
+    },
+    artworkUploadUrl (): string {
+      return config.images.fileuploaderUploadUrl;
+    },
+    getProductBySkuDictionary (): Record<string, Product> {
+      return this.$store.getters['product/getProductBySkuDictionary'];
+    }
+  },
+  async mounted (): Promise<void> {
+    // TODO check ID in URL and load plushie instead of create a new one
+    await this.setCurrentProduct();
+    this.plushieId = await this.createPlushie();
+  },
+  async asyncData ({ store, route, context }): Promise<void> {
+    if (context) context.output.cacheTags.add('product')
+
+    const product = await store.dispatch('product/loadProduct',
+      {
+        parentSku: this.sku,
+        setCurrent: false
+      }
+    );
+
+    await Promise.all([
+      store.dispatch('budsies/loadProductBodyparts', { productId: product.id }),
+      store.dispatch('budsies/loadProductRushAddons', { productId: product.id })
+    ]);
+
+    if (isServer) {
+      await store.dispatch('product/setCurrent', product);
+    }
+
+    catalogHooksExecutors.productPageVisited(product);
+  },
+  beforeRouteLeave (to, from, next) {
+    this.$store.commit(`product/${PRODUCT_UNSET_CURRENT}`);
+    next();
+  },
+  methods: {
+    async onMakeAnother (): Promise<void> {
+      this.plushieId = await this.createPlushie();
+    },
+    async createPlushie (): Promise<number> {
+      if (!this.getCurrentProduct) {
+        throw new Error('Current product is not set!');
+      }
+
+      const task = await this.$store.dispatch('budsies/createNewPlushie', { productId: this.getCurrentProduct.id });
+      return task.result;
+    },
+    async setCurrentProduct (): Promise<void> {
+      if (this.getCurrentProduct) {
+        return;
+      }
+
+      const product = this.getProductBySkuDictionary[this.sku];
+      await this.$store.dispatch('product/setCurrent', product);
+    }
+  },
+  metaInfo (): Record<string, any> {
+    return {
+      title: htmlDecode(
+        this.getCurrentProduct?.meta_title || this.getCurrentProduct?.name
+      ),
+      meta: this.getCurrentProduct?.meta_description
+        ? [
+          {
+            vmid: 'description',
+            name: 'description',
+            content: htmlDecode(this.getCurrentProduct?.meta_description)
+          }
+        ]
+        : []
+    };
+  }
+});
+</script>
+
+  <style lang="scss" scoped>
+  @import "~@storefront-ui/shared/styles/helpers/breakpoints";
+
+  #budsies-plushie-product {
+    box-sizing: border-box;
+    padding: var(--spacer-lg) 1rem 0;
+
+    @media (min-width: $tablet-min) {
+      max-width: 1272px;
+      width: 100%;
+      margin: 0 auto;
+    }
+  }
+
+  </style>
