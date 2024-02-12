ade8c7535ef48a216c26271679a91cf21769ef69
Anton Sylka <antonsylka1004@gmail.com>
2024-02-12 14:31:15 +0700

    Refs #31632: remove redundant types definition

--- types/glide.d.ts
+++
@@ -1,8 +0,0 @@
-import { Options } from '@glidejs/glide';
-
-declare module '@glidejs/glide' {
-  interface Static {
-    /* eslint-disable  @typescript-eslint/no-misused-new */
-    new (selector: string | HTMLElement, options?: Options): Static
-  }
-}
