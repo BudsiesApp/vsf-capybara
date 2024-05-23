ac2bfd7a4beff8d91c8b3cd41a6f781d3ce841f9
Anton Sylka <antonsylka1004@gmail.com>
2024-05-23 15:33:54 +0700

    Refs #32206: CustomizableProduct - refactor to use composition api

--- pages/CustomizableProduct.vue
+++ pages/CustomizableProduct.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="customizable-product">
     <product-structured-data
-      v-if="getCurrentProduct"
-      :product="getCurrentProduct"
+      v-if="currentProduct"
+      :product="currentProduct"
     />
 
     <form-with-images-gallery
-      :product="getCurrentProduct"
+      :product="currentProduct"
       :existing-cart-item="existingCartItem"
       v-if="showForm"
     />
@@ -14,21 +14,19 @@
 </template>
 
 <script lang="ts">
-import Vue, { PropType } from 'vue';
+import { computed, defineComponent, PropType, toRefs } from '@vue/composition-api';
 
-import CartItem from 'core/modules/cart/types/CartItem';
-import Product from 'core/modules/catalog/types/Product';
-import EventBus from '@vue-storefront/core/compatibility/plugins/event-bus';
-import { htmlDecode } from '@vue-storefront/core/filters';
-import { catalogHooksExecutors } from '@vue-storefront/core/modules/catalog-next/hooks';
 import { PRODUCT_UNSET_CURRENT } from '@vue-storefront/core/modules/catalog/store/product/mutation-types';
+import { htmlDecode } from '@vue-storefront/core/filters';
 
 import { ProductStructuredData } from 'src/modules/budsies';
-import { ProductEvent } from 'src/modules/shared';
+
+import { useExistingCartItem } from 'theme/helpers/use-existing-cart-item';
+import { useProductPage } from 'theme/helpers/use-product-page';
 
 import FormWithImagesGallery from 'theme/components/customization-system/forms/form-with-images-gallery.vue';
 
-export default Vue.extend({
+export default defineComponent({
   name: 'CustomizableProduct',
   components: {
     FormWithImagesGallery,
@@ -44,82 +42,31 @@ export default Vue.extend({
       default: undefined
     }
   },
-  data () {
+  setup (props, context) {
+    const { existingPlushieId, sku } = toRefs(props);
+
+    const { currentProduct, isDataLoaded } = useProductPage(sku, context);
+
+    const showForm = computed<boolean>(() => {
+      return isDataLoaded.value && !!currentProduct.value;
+    });
+
     return {
-      isDataLoaded: false
-    };
-  },
-  computed: {
-    cartItems (): CartItem[] {
-      return this.$store.getters['cart/getCartItems'];
-    },
-    existingCartItem (): CartItem | undefined {
-      if (!this.existingPlushieId) {
-        return;
-      }
-
-      return this.cartItems.find((item) => item.plushieId && item.plushieId === this.existingPlushieId);
-    },
-    getCurrentProduct (): Product | null {
-      const product = this.$store.getters['product/getCurrentProduct'];
-      if (!product?.sku || product.sku !== this.sku) {
-        return null;
-      }
-
-      return product;
-    },
-    showForm (): boolean {
-      return this.isDataLoaded && !!this.getCurrentProduct;
+      ...useExistingCartItem(existingPlushieId, context),
+      currentProduct,
+      showForm
     }
-  },
-  async serverPrefetch () {
-    if (this.$ssrContext) this.$ssrContext.output.cacheTags.add('product')
-
-    await (this as any).loadData();
-  },
-  async beforeMount () {
-    if (!this.getCurrentProduct) {
-      await this.loadData();
-    }
-
-    this.isDataLoaded = true;
-    EventBus.$emit(ProductEvent.PRODUCT_PAGE_SHOW, this.getCurrentProduct);
   },
   beforeRouteLeave (to, from, next) {
     this.$store.commit(`product/${PRODUCT_UNSET_CURRENT}`);
     next();
   },
-  methods: {
-    async loadData (): Promise<void> {
-      this.isDataLoaded = false;
-
-      const product = await this.$store.dispatch('product/loadProduct', {
-        parentSku: this.sku,
-        setCurrent: true
-      });
-
-      catalogHooksExecutors.productPageVisited(product);
-
-      await Promise.all([
-        this.$store.dispatch('budsies/loadProductRushAddons', {
-          productId: product.id
-        })
-      ]);
-
-      this.isDataLoaded = true;
-    }
-  },
-  watch: {
-    sku: async function () {
-      await this.loadData();
-    }
-  },
   metaInfo () {
-    const description = this.getCurrentProduct?.meta_description || this.getCurrentProduct?.short_description;
+    const description = this.currentProduct?.meta_description || this.currentProduct?.short_description;
 
     return {
       title: htmlDecode(
-        this.getCurrentProduct?.meta_title || this.getCurrentProduct?.name
+        this.currentProduct?.meta_title || this.currentProduct?.name
       ),
       meta: description
         ? [
@@ -142,14 +89,14 @@ export default Vue.extend({
     box-sizing: border-box;
     padding: 0 1rem;
 
+    .form-with-images-gallery {
+      margin-top: var(--spacer-lg);
+    }
+
     @media (min-width: $tablet-min) {
       max-width: 1272px;
       width: 100%;
       margin: 0 auto;
-
-      .form-with-images-gallery {
-        margin-top: var(--spacer-lg);
-      }
     }
   }
   </style>
