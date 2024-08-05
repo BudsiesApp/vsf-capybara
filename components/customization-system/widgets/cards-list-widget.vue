4b6dc115d0b0d98f86b206c0420205a9bb993261
Andrey Gorbunov <andrey@optimuspro.ru>
2024-08-05 20:07:07 +0200

    Refs #32711: Cards-list widget - keep checkboxes on the top line and move the description under it (WIP)

--- components/customization-system/widgets/cards-list-widget.vue
+++ components/customization-system/widgets/cards-list-widget.vue
@@ -3,7 +3,8 @@
     <ul class="_list">
       <li
         class="_item"
-        v-for="optionValue in sortedValues"
+        :class="{ '--is-selected': isSelected(optionValue) }"
+        v-for="(optionValue, index) in sortedValues"
         :key="optionValue.id"
       >
         <m-checkbox
@@ -12,46 +13,49 @@
           :valid="isValid"
           :value="optionValue.id"
           v-model="selectedOption"
+          ref="checkboxes"
         >
           <template #label>
-            <div class="_wrapper">
-              <div class="_media" v-if="getItemImage(optionValue)">
-                <div class="_image-container">
-                  <base-image
-                    class="_image"
-                    v-if="getItemImage(optionValue)"
-                    :aspect-ratio="1"
-                    :src="getItemImage(optionValue)"
-                  />
-                </div>
+            <div class="sf-checkbox__label _title-wrapper">
+              <div class="_title" v-if="optionValue.name">
+                {{ optionValue.name }}
               </div>
 
-              <div class="_info">
-                <div class="_title" v-if="optionValue.name">
-                  {{ optionValue.name }}
-                </div>
+              <div
+                class="_price"
+                v-if="optionValuePriceDictionary[optionValue.id]"
+              >
+                <strong> + </strong>
 
-                <div
-                  class="_price"
-                  v-if="optionValuePriceDictionary[optionValue.id]"
-                >
-                  <strong> + </strong>
-
-                  <SfPrice
-                    :regular="formatPrice(optionValuePriceDictionary[optionValue.id].regular)"
-                    :special="formatPrice(optionValuePriceDictionary[optionValue.id].special)"
-                  />
-                </div>
-
-                <div
-                  class="_description"
-                  v-html="optionValue.description"
-                  v-if="optionValue.description"
+                <SfPrice
+                  :regular="formatPrice(optionValuePriceDictionary[optionValue.id].regular)"
+                  :special="formatPrice(optionValuePriceDictionary[optionValue.id].special)"
                 />
               </div>
             </div>
           </template>
         </m-checkbox>
+
+        <div
+          class="_description-wrapper"
+        >
+          <div class="_media" v-if="getItemImage(optionValue)">
+            <div class="_image-container">
+              <base-image
+                class="_image"
+                v-if="getItemImage(optionValue)"
+                :aspect-ratio="1"
+                :src="getItemImage(optionValue)"
+              />
+            </div>
+          </div>
+
+          <div
+            class="_description"
+            v-html="optionValue.description"
+            v-if="optionValue.description"
+          />
+        </div>
       </li>
     </ul>
 
@@ -133,6 +137,8 @@ export default defineComponent({
       ...useOptionValuesPrice(values, context),
       ...useValuesSort(values)
     };
+  },
+  methods: {
   }
 });
 </script>
@@ -151,25 +157,25 @@ export default defineComponent({
 
   ._item {
     cursor: pointer;
-  }
-
-  ._checkbox {
     padding: var(--spacer-sm);
     transition: background-color 0.15s cubic-bezier(0.65, 0.05, 0.35, 1);
 
-    &.sf-checkbox--is-active {
+    &.--is-selected {
       background-color: var(--c-secondary);
     }
-
-    ::v-deep .sf-checkbox__container {
-      align-items: flex-start;
-    }
   }
 
-  ._wrapper {
-    margin-left: var(--spacer-sm);
-    text-align: left;
+  ._checkbox {
+    --checkbox-font-size: var(--font-size-base);
   }
+
+  ._title-wrapper {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    gap: var(--spacer-xs);
+  }
+
   ._title {
     font-weight: var(--font-semibold);
     margin-top: calc(var(--checkbox-size, 1.5rem) / 10);
@@ -193,10 +199,19 @@ export default defineComponent({
     }
   }
 
+  ._description-wrapper {
+    text-align: left;
+    display: flex;
+    flex-direction: row;
+    flex-grow: 1;
+    gap: var(--spacer-xs);
+    margin-top: var(--spacer-xs);
+  }
+
   ._media {
-    // float: right;
-    width: 40%;
-    padding: 0 var(--spacer-sm) var(--spacer-sm) 0;
+    width: 30%;
+    flex-shrink: 0;
+    flex-grow: 1;
 
     ._image-container {
       position: relative;
@@ -210,7 +225,6 @@ export default defineComponent({
 
   ._description {
     font-size: var(--font-sm);
-    margin-top: var(--spacer-sm);
 
     > :first-child {
       margin-top: 0;
@@ -232,16 +246,8 @@ export default defineComponent({
   }
 
   @include for-desktop {
-    ._wrapper {
-      display: flex;
-      flex-direction: row;
-      flex-grow: 1;
-    }
-
     ._media {
       width: 25%;
-      flex-shrink: 0;
-      flex-grow: 1;
     }
   }
 }
