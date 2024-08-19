0dc27141e6ed04de1886161e9f7ac1f48f1b855d
Anton Sylka <antonsylka1004@gmail.com>
2024-08-19 15:56:28 +0700

    Refs #32874: Cards List Widget - remove redundant ref attribute

--- components/customization-system/widgets/cards-list-widget.vue
+++ components/customization-system/widgets/cards-list-widget.vue
@@ -12,7 +12,6 @@
           :valid="isValid"
           :value="optionValue.id"
           v-model="selectedOption"
-          ref="checkboxes"
         >
           <template #checkmark="{ isChecked }">
             <div class="_checkmark-container">
