a4e4dba3f41cbc6eb018c73a8a41a18df2685593
Anton Sylka <antonsylka1004@gmail.com>
2024-11-08 15:16:13 +0700

    Refs #33388: Address Forms - use state search by code for autocomplete value

--- components/molecules/m-multiselect.vue
+++ components/molecules/m-multiselect.vue
@@ -24,6 +24,7 @@
       :show-no-results="!allowFreeText"
       :max-height="190"
       :autocomplete="autocomplete"
+      :autocomplete-value-search="autocompleteValueSearch"
       open-direction="below"
       :disabled="disabled"
       ref="multiselect"
@@ -163,6 +164,10 @@ export default Vue.extend({
     hideDropdownArrow: {
       type: Boolean,
       default: false
+    },
+    autocompleteValueSearch: {
+      type: Function as PropType<((option: any, value: string) => boolean) | undefined>,
+      default: undefined
     }
   },
   data () {
