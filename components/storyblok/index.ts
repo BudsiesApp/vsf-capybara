68b5926f59bf8b4cfbc0c52cda1afe52585f9719
Anton Sylka <antonsylka1004@gmail.com>
2025-02-21 17:33:17 +0800

    Refs #34015: Storyblok Images Preview - implement component

--- components/storyblok/index.ts
+++ components/storyblok/index.ts
@@ -24,6 +24,7 @@ export default function registerComponents () {
   add('tabs', () => import(/* webpackChunkName: "vsf-storyblok" */ './Tabs.vue'))
   add('tab', () => import(/* webpackChunkName: "vsf-storyblok" */ './Tab.vue'))
   add('carousel', () => import(/* webpackChunkName: "vsf-storyblok" */ './Carousel.vue'))
+  add('images_preview', () => import(/* webpackChunkName: "vsf-storyblok" */ './ImagesPreview.vue'))
 
   // add('product', () => import('./Product.vue'))
   // add('category', () => import('./Category.vue'))
