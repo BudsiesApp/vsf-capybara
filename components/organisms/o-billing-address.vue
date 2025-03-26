c4462714eb7d486e448b1192be3e5daddc24d690
Anton Sylka <antonsylka1004@gmail.com>
2025-03-26 19:17:19 +0800

    Refs #34241: Checkout | Address forms - fix state clear when country changes

--- components/organisms/o-billing-address.vue
+++ components/organisms/o-billing-address.vue
@@ -89,7 +89,7 @@
           :valid="!$v.payment.country.$error"
           :error-message="$t('Field is required')"
           :disabled="isFormFieldsDisabled"
-          @change="changeCountry"
+          @change="onChangeCountry"
         />
 
         <SfInput
@@ -383,11 +383,16 @@ export default {
   },
   methods: {
     stateCodeAutocompleteOptionSearch,
-    async changeCountry () {
+    async onChangeCountry () {
+      await this.changeCountry();
       await this.$nextTick();
-      this.payment.state = '';
-      this.validateCountryRelatedFields();
 
+      this.payment.state = '';
+      this.payment.region_id = null;
+
+      this.validateCountryRelatedFields();
+    },
+    async changeCountry () {
       await Promise.all([
         this.$store.dispatch('checkout/updatePaymentDetails', { country: this.payment.country }),
         this.$store.dispatch('cart/syncPaymentMethods', { forceServerSync: true })
@@ -447,11 +452,6 @@ export default {
   },
   watch: {
     getPaymentCountry (after, before) {
-      if (after && before && after !== before) {
-        this.payment.region_id = null;
-        this.payment.state = '';
-      }
-
       if (after && before !== after) {
         this.changeCountry();
       }
