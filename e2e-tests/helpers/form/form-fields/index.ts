b2f73db39fc20f361ec8a3703f61fcca9b047643
Anton Sylka <antonsylka1004@gmail.com>
2025-03-28 15:08:22 +0800

    Refs #34221: Remove redundant code

--- e2e-tests/helpers/form/form-fields/index.ts
+++ e2e-tests/helpers/form/form-fields/index.ts
@@ -1,11 +1,9 @@
 import { InputFormField } from './input-form-field';
 import { MultiselectFormField } from './multiselect-form-field';
 import { SelectFormField } from './select-form-field';
-import { TextAreaFormField } from './textarea-form-field';
 
 export {
   InputFormField,
   MultiselectFormField,
-  SelectFormField,
-  TextAreaFormField
+  SelectFormField
 }
