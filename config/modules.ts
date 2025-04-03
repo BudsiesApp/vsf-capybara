edf82ea26cf3d354c3e2f1ae9f23072312809bf9
Anton Sylka <antonsylka1004@gmail.com>
2025-04-03 11:19:02 +0800

    Refs #34291: A/B Testing - fix modules inititalization order

--- config/modules.ts
+++ config/modules.ts
@@ -14,6 +14,7 @@ import { StoryblokModule } from 'src/modules/vsf-storyblok-module'
 import { forStoryblok } from 'src/modules/vsf-storyblok-module/mappingFallback'
 import { extendStore } from '@vue-storefront/core/helpers'
 import { StorefrontModule, registerModule } from '@vue-storefront/core/lib/modules'
+import { ABTesting } from 'src/modules/a-b-testing'
 import { BudsiesModule } from 'src/modules/budsies'
 import { PromotionPlatformModule } from 'src/modules/promotion-platform'
 import { GiftCardModule } from 'src/modules/gift-card'
@@ -28,7 +29,6 @@ import { PersistedCustomerDataModule } from 'src/modules/persisted-customer-data
 import { TrueVaultModule } from 'src/modules/true-vault';
 
 import registerStoryblokComponents from 'theme/components/storyblok'
-import { ABTesting } from 'src/modules/a-b-testing'
 
 const extendUrlVuex = {
   actions: {
@@ -54,7 +54,6 @@ const extendUrlModule: StorefrontModule = function ({ store }) {
 // TODO:distributed across proper pages BEFORE 1.11
 export function registerClientModules () {
   registerStoryblokComponents()
-  registerModule(ABTesting)
   registerModule(TrueVaultModule)
   registerModule(PageLoadingIndicatorModule)
   registerModule(UrlModule)
@@ -70,6 +69,7 @@ export function registerClientModules () {
   registerModule(extendUrlModule)
   registerModule(BudsiesModule)
   registerModule(GoogleTagManagerModule)
+  registerModule(ABTesting)
   registerModule(PromotionPlatformModule)
   registerModule(GiftCardModule)
   registerModule(PaymentBackendMethodsModule)
