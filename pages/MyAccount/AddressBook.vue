accec066a4e3f922d9c4e2a1df4eff4f610f5508
Anton Sylka <antonsylka1004@gmail.com>
2025-05-20 17:45:02 +0800

    Refs #34611: Address Book - refactor navigation to use router

---
+++ pages/MyAccount/AddressBook.vue
@@ -0,0 +1,35 @@
+<template>
+  <SfTabs :open-tab="1" id="address-book-page">
+    <SfTab :title="tabTitle" class="_tab">
+      <router-view />
+    </SfTab>
+  </SfTabs>
+</template>
+
+<script lang="ts">
+import { SfTabs } from '@storefront-ui/vue';
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'AddressBook',
+  props: {
+    tabTitle: {
+      type: String,
+      default: 'Address Book'
+    }
+  },
+  components: {
+    SfTabs
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+@import "~@storefront-ui/shared/styles/helpers/breakpoints";
+
+#address-book-page {
+  @include for-mobile {
+    --tabs-title-display: none;
+  }
+}
+</style>
