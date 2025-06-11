19fa9815c32312da73c2fbfb45bc87139ed50a37
Anton Sylka <antonsylka1004@gmail.com>
2025-06-11 14:55:03 +0800

    Refs #34473: Root Component - handle currency exchange rate change

--- App.vue
+++ App.vue
@@ -17,11 +17,13 @@ import { ModalList } from './store/ui/modals';
 import { FileProcessingRepositoryFactory, ImageHandlerService, itemFactory } from 'src/modules/file-storage'
 import { ErrorConverterService } from 'src/modules/budsies'
 import { isServer } from '@vue-storefront/core/helpers'
+import { CART_SET_EXCHANGE_RATE_MUTATION } from '@vue-storefront/core/modules/cart';
+import { SET_EXCHANGE_RATE_MUTATION } from '@vue-storefront/core/modules/catalog';
 import EventBus from '@vue-storefront/core/compatibility/plugins/event-bus'
 import { USER_LEAVING_WEBSITE } from 'src/modules/promotion-platform';
 import { isStoryblokPreview } from 'src/modules/vsf-storyblok-module';
 import { SN_PROMOTION_PLATFORM } from 'src/modules/promotion-platform/types/StoreMutations';
-import { FETCH_AVAILABLE_CURRENCIES_ACTION, FETCH_CURRENCY_RATES_ACTION } from 'src/modules/currency';
+import { FETCH_AVAILABLE_CURRENCIES_ACTION, FETCH_CURRENCY_RATES_ACTION, GET_CURRENCY_EXCHANGE_RATE } from 'src/modules/currency';
 
 const windowObject = isServer ? {} : window;
 const errorConverterService = new ErrorConverterService();
@@ -40,6 +42,9 @@ export default {
   computed: {
     layout () {
       return `${get(this.$route, 'meta.layout', 'default')}-layout`
+    },
+    currencyExchangeRate () {
+      return this.$store.getters[GET_CURRENCY_EXCHANGE_RATE];
     }
   },
   mounted () {
@@ -81,6 +86,15 @@ export default {
       }
 
       this.$store.dispatch('ui/openModal', { name: ModalList.WebsiteLeaving })
+    }
+  },
+  watch: {
+    currencyExchangeRate: {
+      handler (exchangeRate) {
+        this.$store.commit(CART_SET_EXCHANGE_RATE_MUTATION, exchangeRate);
+        this.$store.commit(SET_EXCHANGE_RATE_MUTATION, exchangeRate);
+      },
+      immediate: true
     }
   }
 };
