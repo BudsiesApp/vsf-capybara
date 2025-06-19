add935f4aec3e764c9b71b17596c9b0ce535c9f7
Anton Sylka <antonsylka1004@gmail.com>
2025-06-19 15:50:19 +0800

    Refs #34453: Google Tag Manager - update customer data changed event data

--- App.vue
+++ App.vue
@@ -22,6 +22,7 @@ import { USER_LEAVING_WEBSITE } from 'src/modules/promotion-platform';
 import { isStoryblokPreview } from 'src/modules/vsf-storyblok-module';
 import { SN_PROMOTION_PLATFORM } from 'src/modules/promotion-platform/types/StoreMutations';
 import { FETCH_AVAILABLE_CURRENCIES_ACTION, FETCH_CURRENCY_RATES_ACTION, GET_CURRENCY_EXCHANGE_RATE } from 'src/modules/currency';
+import { notifyCustomerDataChanged, CUSTOMER_DATA, CUSTOMER_DATA_HASH } from 'src/modules/persisted-customer-data';
 
 const windowObject = isServer ? {} : window;
 const errorConverterService = new ErrorConverterService();
@@ -40,6 +41,12 @@ export default {
   computed: {
     layout () {
       return `${get(this.$route, 'meta.layout', 'default')}-layout`
+    },
+    customerData () {
+      return this.$store.getters[CUSTOMER_DATA];
+    },
+    customerDataHash () {
+      return this.$store.getters[CUSTOMER_DATA_HASH];
     }
   },
   mounted () {
@@ -81,6 +88,18 @@ export default {
       }
 
       this.$store.dispatch('ui/openModal', { name: ModalList.WebsiteLeaving })
+    }
+  },
+  watch: {
+    customerDataHash: {
+      handler (newHash, oldHash) {
+        if (isServer || newHash === oldHash) {
+          return;
+        }
+
+        notifyCustomerDataChanged(this.customerData);
+      },
+      immediate: true
     }
   }
 };
