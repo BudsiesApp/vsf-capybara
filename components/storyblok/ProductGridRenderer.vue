847a63e5ae1b209406593fb0fada22d881627c51
Anton Sylka <antonsylka1004@gmail.com>
2025-12-04 17:45:43 +0800

    Refs #35780: Product Grid Renderer - use `route.path` as category name and id

--- components/storyblok/ProductGridRenderer.vue
+++ components/storyblok/ProductGridRenderer.vue
@@ -104,7 +104,7 @@ export default defineComponent({
         return props.contextName;
       }
 
-      return root.$route.fullPath;
+      return root.$route.path;
     });
 
     function onProductCardClick (productSku: string): void {
