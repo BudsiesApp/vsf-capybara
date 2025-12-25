586872bcefb0eb90f3d8a62f500a0e68da7e779f
Anton Sylka <antonsylka1004@gmail.com>
2025-12-25 13:47:41 +0800

    Refs #35910: Base Address Form - update isStateHidden helper usage

--- components/organisms/o-base-address-form.vue
+++ components/organisms/o-base-address-form.vue
@@ -256,7 +256,7 @@ import { stateCodeAutocompleteOptionSearch, createPhoneHelpers } from 'src/modul
 import BaseAddressDetails from '@vue-storefront/core/modules/checkout/types/BaseAddressDetails';
 import { useAddressAutocomplete } from 'src/modules/address/composables/use-address-autocomplete';
 import { googleMapsAttributionLogo } from 'src/modules/address';
-import { isStateHidden as checkIfStateHidden } from 'src/modules/address/helpers/is-state-hidden';
+import { isStateNonPostal } from 'src/modules/address/helpers/is-state-non-postal';
 
 import MMultiselect from 'theme/components/molecules/m-multiselect.vue';
 import MSuggestionsList from 'theme/components/molecules/m-suggestions-list.vue';
@@ -452,7 +452,7 @@ export default defineComponent({
     });
 
     const isStateHidden = computed<boolean>(() => {
-      return checkIfStateHidden(country.value);
+      return isStateNonPostal(country.value);
     });
 
     const phoneValidationRules = computed<any>(() => {
