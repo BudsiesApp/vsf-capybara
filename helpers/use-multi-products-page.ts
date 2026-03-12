d992f132063aaea90520bdfa8332e45c4c652e91
Anton Sylka <antonsylka1004@gmail.com>
2026-03-12 13:17:14 +0800

    Refs #36331: Forevers Customize Later - move products loading into a separate composable

---
+++ helpers/use-multi-products-page.ts
@@ -0,0 +1,102 @@
+import { computed, onBeforeMount, onServerPrefetch, ref, Ref, SetupContext } from '@vue/composition-api';
+import config from 'config';
+import { SearchQuery } from 'storefront-query-builder';
+
+import EventBus from '@vue-storefront/core/compatibility/plugins/event-bus';
+import { PRODUCT_UNSET_CURRENT } from '@vue-storefront/core/modules/catalog/store/product/mutation-types';
+import { catalogHooksExecutors } from '@vue-storefront/core/modules/catalog-next/hooks';
+import Product from 'core/modules/catalog/types/Product';
+import { ProductEvent } from 'src/modules/shared';
+import { updateProductProductionTimeCustomizationData } from 'src/modules/customization-system';
+
+export function useMultiProductsPage (
+  skus: Ref<string[]>,
+  { root }: SetupContext
+) {
+  const isDataLoaded = ref<boolean>(false);
+
+  function getSearchQuery (productSkus: string[]) {
+    let productsQuery = new SearchQuery();
+
+    productsQuery = productsQuery
+      .applyFilter({ key: 'sku', value: { in: productSkus } })
+      .applyFilter({ key: 'status', value: { in: [1] } });
+
+    if (config.products.listOutOfStockProducts === false) {
+      productsQuery = productsQuery.applyFilter({ key: 'stock.is_in_stock', value: { eq: true } });
+    }
+
+    return productsQuery;
+  }
+
+  const currentProduct = computed<Product | undefined>(() => {
+    const product = root.$store.getters['product/getCurrentProduct'];
+
+    if (!product?.sku) {
+      return null;
+    }
+
+    return product;
+  });
+
+  const productBySkuDictionary = computed<Record<string, Product>>(() => {
+    return root.$store.getters['product/getProductBySkuDictionary'] || {};
+  });
+
+  async function selectProduct (sku: string): Promise<void> {
+    const product = productBySkuDictionary.value[sku];
+
+    if (!product) {
+      return;
+    }
+
+    const productUpdated = updateProductProductionTimeCustomizationData(product, root.$store);
+    root.$store.commit('product/product/PRODUCT_SET_PRODUCT_BY_SKU', productUpdated);
+    await root.$store.dispatch('product/setCurrent', product);
+  }
+
+  async function loadData (): Promise<void> {
+    isDataLoaded.value = false;
+    root.$store.commit(`product/${PRODUCT_UNSET_CURRENT}`);
+
+    await root.$store.dispatch('product/findProducts', {
+      query: getSearchQuery(skus.value),
+      options: {
+        prefetchGroupProducts: false
+      }
+    });
+
+    await selectProduct(skus.value[0]);
+
+    if (currentProduct.value) {
+      catalogHooksExecutors.productPageVisited(currentProduct.value);
+    }
+
+    isDataLoaded.value = true;
+  }
+
+  onServerPrefetch(async () => {
+    await loadData();
+  });
+
+  onBeforeMount(async () => {
+    if (!currentProduct.value) {
+      await loadData();
+    }
+
+    isDataLoaded.value = true;
+
+    if (!currentProduct.value) {
+      return;
+    }
+
+    EventBus.$emit(ProductEvent.PRODUCT_PAGE_SHOW, currentProduct.value);
+  });
+
+  return {
+    currentProduct,
+    isDataLoaded,
+    productBySkuDictionary,
+    selectProduct
+  };
+}
