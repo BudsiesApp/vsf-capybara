7e0fc88a949fc95c44b44aa9f7f9a9bc988e3f9f
Anton Sylka <antonsylka1004@gmail.com>
2026-06-04 17:55:11 +0800

    Refs #36725: Traffic Attribution - enable module

--- config/modules.ts
+++ config/modules.ts
@@ -30,6 +30,7 @@ import { TrueVaultModule } from 'src/modules/true-vault';
 import { OrdersHistoryModule } from 'src/modules/orders-history';
 import { CurrencyModule } from 'src/modules/currency';
 import { FeraModule } from 'src/modules/fera';
+import { TrafficAttributionModule } from 'src/modules/traffic-attribution';
 
 import registerStoryblokComponents from 'theme/components/storyblok'
 import { KlaviyoModule, ModuleConfig } from 'src/modules/klaviyo'
@@ -88,6 +89,7 @@ export function registerClientModules () {
   registerModule(CurrencyModule)
   registerModule(FeraModule)
   registerModule(AmazonPay)
+  registerModule(TrafficAttributionModule)
 
   const klaviyoModuleConfig: ModuleConfig = {
     updateEmailMutation: SET_PERSISTED_CUSTOMER_EMAIL,
