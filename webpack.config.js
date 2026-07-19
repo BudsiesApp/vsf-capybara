041ca2ca271f14f4e9d507dee0c94885439c7d04
Andrey Gorbunov <andrey@optimuspro.ru>
2026-07-20 00:12:21 +0200

    Refs #36838: Update packages to Node 24-compatible versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 // You can extend default webpack build here.
 // Read more in docs: https://github.com/DivanteLtd/vue-storefront/blob/master/docs/guide/core-themes/webpack.md
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const themeRoot = require('@vue-storefront/core/build/theme-path');
 
 module.exports = function (config, { isClient }) {
