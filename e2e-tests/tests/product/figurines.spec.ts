3f25c0e13f50a9bee376699ce7e45f3d7b5cf46d
Anton Sylka <antonsylka1004@gmail.com>
2026-07-22 12:10:48 +0800

    Refs #36663: Forevers | Figurines - update tests to respect hidden description customization on the cart page

--- e2e-tests/tests/product/figurines.spec.ts
+++ e2e-tests/tests/product/figurines.spec.ts
@@ -76,7 +76,6 @@ test('product display in cart correctly', async ({ cartPage, customizableProduct
   const cartItem = cartPage.getCartItemByProductName(PRODUCT_NAME);
 
   await expect(cartItem).toBeVisible();
-  await cartPage.expectCartItemToHaveProperties(cartItem, [DESCRIPTION_TEXT]);
 });
 
 test('product can be edited', async ({ cartPage, customizableProductPage }) => {
@@ -95,10 +94,6 @@ test('product can be edited', async ({ cartPage, customizableProductPage }) => {
 
   await customizableProductPage.addToCartAndVerifyResponse();
   await cartPage.waitPageToBeVisible();
-
-  await cartPage.goto();
-  const updatedCartItem = cartPage.getCartItemByProductName(PRODUCT_NAME);
-  await cartPage.expectCartItemToHaveProperties(updatedCartItem, [UPDATED_DESCRIPTION_TEXT]);
 });
 
 test('form fields are reset after save and make another', async ({ page, customizableProductPage, verticalStepsProductPage }) => {
